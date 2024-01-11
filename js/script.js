d849e5c99f3755662c26984d61892a749d031f21
Iann Domenes <74912076+iann-domenes@users.noreply.github.com>
2024-01-11 19:17:57 +0000

    projeto base

--- js/script.js
+++ js/script.js
@@ -1,14 +1,59 @@
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
+const score = document.querySelector(".score--value")
+const finalScore = document.querySelector(".final-score > span")
+const menu = document.querySelector(".menu-screen")
+const buttonPlay = document.querySelector(".btn-play")
+
+const audio = new Audio("../assets/audio.mp3")
+
 const size = 30
 
-const snake = [
-    {  x: 200, y: 200 },
-    {  x: 230, y: 200 }
-]
+const initialPosition = {  x: 270, y: 240 }
+
+let snake = [initialPosition]
 
 let direction, loopId
+
+const incrementScore = () => {
+    score.innerText = +score.innerText + 10
+}
+
+const randomNumber = (min, max) => {
+    return Math.round(Math.random() * (max - min) + min)
+}
+
+const randomPosition = () => {
+    const number = randomNumber(0, canvas.width - size)
+    return Math.round(number / 30) * 30
+}
+
+const randomColor = () => {
+    const red = randomNumber(0, 255)
+    const green = randomNumber(0, 255)
+    const blue = randomNumber(0, 255)
+
+    return `rgb(${red}, ${green}, ${blue})`
+}
+
+const food = {
+    x: randomPosition(),
+    y: randomPosition(),
+    color: randomColor()
+}
+
+const drawFood = () => {
+
+    const {x, y, color} = food
+
+    ctx.shadowColor = color
+    ctx.shadowBlur = 10
+    ctx.fillStyle = color
+    ctx.fillRect(x, y, size, size)
+    ctx.shadowBlur = 0
+
+}
 
 const drawSnake = () => {
     
@@ -23,6 +68,23 @@ const drawSnake = () => {
         ctx.fillRect(position.x, position.y, size, size)
     })
 
+}
+
+const drawGrid = () => {
+    ctx.lineWidth = 1
+    ctx.strokeStyle = "#191919"
+
+    for (let i = 30; i < canvas.width; i += 30) {
+        ctx.beginPath()
+        ctx.lineTo(i, 0)
+        ctx.lineTo(i, 600)
+        ctx.stroke()
+
+        ctx.beginPath()
+        ctx.lineTo(0, i)
+        ctx.lineTo(600, i)
+        ctx.stroke()
+    }
 }
 
 const moveSnake = () => {
@@ -48,12 +110,62 @@ const moveSnake = () => {
     snake.shift()
 }
 
+const checkEat = () => {
+    const head = snake[snake.length -1]
+
+    if(head.x == food.x && head.y == food.y) {
+        incrementScore()
+        snake.push(head)
+        audio.play()
+
+        let x = randomPosition()
+        let y = randomPosition()
+
+        while(snake.find((position) => position.x == x && position.y == y)){
+            x = randomPosition()
+            y = randomPosition() 
+        }
+
+        food.x = x
+        food.y = y
+        food.color = randomColor()
+    }
+}
+
+const checkColision = () => {
+    const head = snake[snake.length -1]
+    const canvasLimit = canvas.width - size
+    const neckIndex = snake.length-2
+
+    const wallCollision = head.x < 0 || head.x > canvasLimit || head.y < 0 || head.y > canvasLimit
+
+    const selfCollision = snake.find((position, index) => {
+        return index < neckIndex && position.x == head.x && position.y == head.y
+    })
+
+    if(wallCollision || selfCollision){
+        gameOver()
+    }
+}
+
+const gameOver = () => {
+    direction = undefined
+
+    menu.style.display = "flex"
+    finalScore.innerText = score.innerText
+    canvas.style.filter = "blur(2pt)"
+}
+
 const gameLoop = () => {
     clearInterval(loopId)
 
     ctx.clearRect(0, 0, 600, 600)
+    drawGrid()
+    drawFood()
     moveSnake()
     drawSnake()
+    checkEat()
+    checkColision()
 
     loopId = setTimeout(() => {
         gameLoop()
@@ -61,3 +173,27 @@ const gameLoop = () => {
 }
 
 gameLoop()
+
+document.addEventListener("keydown", ({key}) => {
+    if(key == "ArrowRight" && direction != "left") {
+        direction = "right"
+    }
+    if(key == "ArrowLeft" && direction != "right") {
+        direction = "left"
+    }
+    if(key == "ArrowDown" && direction != "up") {
+        direction = "down"
+    }
+    if(key == "ArrowUp" && direction != "down") {
+        direction = "up"
+    }
+})
+
+buttonPlay.addEventListener("click", () => {
+    score.innerText = "00"
+    menu.style.display = "none"
+    canvas.style.filter = "none"
+
+    snake =[initialPosition]
+
+})
